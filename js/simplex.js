2a0a1c89fafd56e015b1e3d201b32f90715d6da5
Vinicius Dufloth <vinicius.dufloth@gmail.com>
2019-11-17 16:18:51 -0300

    feito partes

--- js/simplex.js
+++ js/simplex.js
@@ -11,29 +11,6 @@ let nTabelas
 let otima
 let lIntermediariaAux
 
-/* resultado 1120
-varColunas = ['Z', 'x1', 'x2', 'x3', 'x4', 'xF1', 'xF2', 'xF3', 'b']
-let linhaTabela1 = [1, -5, 3, -4, 1, 0, 0, 0, 0]
-let linhaTabela2 = [0, 1, 1, 1, 1, 1, 0, 0, 600]
-let linhaTabela3 = [0, 2, 0, 1, 0, 0, 1, 0, 280]
-let linhaTabela4 = [0, 1, 0, 0, 3, 0, 0, 1, 150]
-dadosTabela = [linhaTabela1, linhaTabela2, linhaTabela3, linhaTabela4]
-*/
-
-function prepararValores() {
-    varColunas = ['Z', 'x1', 'x2', 'x3', 'x4', 'xF1', 'xF2', 'xF3', 'b']
-    let linhaTabela1 = [1, -5, 3, -4, 1, 0, 0, 0, 0]
-    let linhaTabela2 = [0, 1, 1, 1, 1, 1, 0, 0, 600]
-    let linhaTabela3 = [0, 2, 0, 1, 0, 0, 1, 0, 280]
-    let linhaTabela4 = [0, 1, 0, 0, 3, 0, 0, 1, 150]
-    dadosTabela = [linhaTabela1, linhaTabela2, linhaTabela3, linhaTabela4]
-    oldTabela = dadosTabela
-    nColunas = dadosTabela[0].length
-    NLP = []
-    nTabelas = 0
-    otima = false
-}
-
 function encontrarPivos() {
     colunaPivo = 0
     // Coluna Pivô = O menor valor da primeira linha
@@ -249,12 +226,81 @@ function calcularVBS() {
     calculosabela.appendChild(tableVB)
 }
 
-function calcularSimplex(e) {
-  //  e.preventDefault();
+function reformularRestricoes(e) {
+    e.preventDefault()
+    //gerar novas linhas e prencher a tabela
+    var num_variavies = document.getElementById('num_variavies').value;
+    var num_restricoes = document.getElementById('num_restricoes').value;
+    varColunas = ['Z']
+    for(i=1; i <= num_variavies; i++){
+        varColunas.push('X'+i);
+    }
+    for(i=1; i <= (parseInt(num_restricoes)+1); i++){
+        varColunas.push('XF'+i);
+    }
+    varColunas.push('b');
+    nColunas = varColunas.length
+    dadosTabela = [];
+    for (i = 0; i <= num_restricoes; i++) {
+        dadosTabela.push(new Array(nColunas));
+    }
+    //Primeira Coluna
+    dadosTabela[0][0] = 1
+    for (i = 1; i <= num_restricoes; i++) {
+        dadosTabela[i][0] = 0
+    }//end Primeira coluna   
+    var valoresExpressoes = []
+    document.getElementById("valores_form")
+            .querySelectorAll("input").forEach((input) => {
+                valoresExpressoes.push(input.value)
+            })
+    //colunas do meio
+    console.log(valoresExpressoes)
+    for (i=0;i <=num_restricoes; i++){
+        for(j=1; j<nColunas; j++){
+            if (i == 0) { //linha Z
+                if (j <= (num_restricoes)) {
+                    dadosTabela[i][j] = valoresExpressoes[j-1]
+                } else {
+                    dadosTabela[i][j] = 0
+                }
+            } else { //outras linhas
+                if (j <= (num_restricoes)) {
+                    dadosTabela[i][j] = valoresExpressoes[(/*calculo maluco*/)]
+                } else { // 0 menos na do seu xf1
+                    dadosTabela[i][j] = 0
+                }
+            }
+        }
+    }//end colunas do meio
+    console.log(dadosTabela)
+
+    NLP = []
+    nTabelas = 0
+    otima = false
+    document.getElementById('novas_restricoes').innerHTML += '<button class="btn btn-primary" onclick="calcularSimplex()">Calcular Tabelas</button>'
+}
+
+function valoresTeste(){
+    /// DEVO PREECNEHR
+    varColunas = ['Z', 'x1', 'x2', 'x3', 'x4', 'xF1', 'xF2', 'xF3', 'b']
+    let linhaTabela1 = [1, -5, 3, -4, 1, 0, 0, 0, 0]
+    let linhaTabela2 = [0, 1, 1, 1, 1, 1, 0, 0, 600]
+    let linhaTabela3 = [0, 2, 0, 1, 0, 0, 1, 0, 280]
+    let linhaTabela4 = [0, 1, 0, 0, 3, 0, 0, 1, 150]
+    dadosTabela = [linhaTabela1, linhaTabela2, linhaTabela3, linhaTabela4]
+    oldTabela = dadosTabela
+    nColunas = dadosTabela[0].length
+    NLP = []
+    nTabelas = 0
+    otima = false
+}
+
+function calcularSimplex() {
     let calculosabela = document.getElementById("calculos")
     calculosabela.innerHTML = ''
 
-    prepararValores()
+    valoresTeste()
     while (!otima) {
         encontrarPivos()
         mostrarTabela()
